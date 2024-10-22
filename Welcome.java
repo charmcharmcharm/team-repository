c5e2ba50df877c62e779d1692e018ca9f5106fce
fionn-og <g00467441@atu.ie>
2024-10-22 15:46:08 +0100

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,7 +1,9 @@
 // for github lab 2
+
+
 public class Welcome{
  public static void main(String[] args){
  System.out.println("Welcome to Github lab 2!");
- System.out.println("lpB now collaborating"); 
+ System.out.println("lpA making changes");
  }
 }
